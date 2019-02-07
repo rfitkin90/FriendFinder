acd2f9192a8c75c5649ecd5709b8cf6e869a76bc
Richard Fitkin <rfitkin90@gmail.com>
2019-02-06 21:03:01 -0800

    2-6 class finished

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var friendsData = require('../data/friends.js')
+
+module.exports = function (app) {
+
+   app.get("/api/friends", function (req, res) {
+      res.json(tableData);
+   });
+
+   app.post("/api/friends", function (req, res) {
+      friendsData.push(req.body);
+      res.json(true);
+   });
+
+}
--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var path = require('path');
 
+var app = express();
+
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+   console.log(`App listening on PORT: ${PORT}`);
+});
